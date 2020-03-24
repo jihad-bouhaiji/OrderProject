ae6d90a84ee0f146b20a7a69841f207e3e1019e2
Jihad Bouhaiji <jihad.bouhaiji@tvh.com>
2020-03-24 15:16:21 +0100

    change git ignore

--- src/test/java/com/tvh/bootcamp/config/SecurityConfiguration.java
+++ src/test/java/com/tvh/bootcamp/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.tvh.bootcamp.config;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
